75b42ad72394f0b164ec9426774a7947973ab414
Ibrahim Abdulmojeed <ibraheemabdulmojeed1@gmail.com>
2023-09-10 01:29:42 +0100

    Initial commit

---
+++ ZuriApi/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
+import datetime
+
+# Create your views here.
+
+def info_render(request):
+    date = datetime.datetime.now()
+    slack_name = request.GET.get('slack_name',)
+    track = request.GET.get('track', None)
+
+    if request.method == 'GET':
+        slack_name = slack_name
+        current_day = date.strftime('%A')
+        utc_time = date.strftime('%Y-%m-%dT%H:%M:%SZ')
+        track = track
+        github_file_url = None
+        github_repo_url = None
+        status_code = 200
+
+        context = {
+            'slack_name': slack_name,
+            'current_day': current_day,
+            'utc_time': utc_time,
+            'track': track,
+            'github_file_url': github_file_url,
+            'github_repo_url': github_repo_url,
+            'status_code': status_code
+        }
+        return JsonResponse(context)
+    
+    else:
+        return JsonResponse(status=403)
