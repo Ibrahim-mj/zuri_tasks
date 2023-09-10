0f398850c5ab92f291a9df3c67c2b6df477ac153
Ibrahim Abdulmojeed <ibraheemabdulmojeed1@gmail.com>
2023-09-10 09:28:53 +0100

    God knows

--- ZuriApi/urls.py
+++ ZuriApi/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/', views.info_render, name='info_render'),
+    path('', views.info_render, name='info_render'),
 ]
